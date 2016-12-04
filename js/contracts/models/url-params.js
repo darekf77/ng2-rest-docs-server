fed79a1fc744670018f380e81b238763af3ae071
Dariusz Filipiak <darekf77@gmail.com>
2016-12-04 10:34:37 +0100

    js removed

--- js/contracts/models/url-params.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-[];
-//# sourceMappingURL=url-params.js.map
--- js/contracts/transform/consts.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.WITHOUT_FORM_LENGTH_INDICATOR = 255;
-exports.PRODUCER = 'producer';
-exports.CONSUMER = 'consumer';
-//# sourceMappingURL=consts.js.map
